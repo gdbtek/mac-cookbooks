a2b28c2d62b5cfa0e406e135e5d8a988594217b7
Nam Nguyen <namnguye@adobe.com>
2014-04-21 23:24:09 -0700

    add Redis cookbook

---
+++ cookbooks/redis/attributes/default.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+downloadURL='http://download.redis.io/releases/redis-stable.tar.gz'
+
+port=6379
+
+installBinFolder='/Volumes/Data/Applications/Redis/bin'
+installConfigFolder='/Volumes/Data/Applications/Redis/config'
+installDataFolder='/Volumes/Data/Data/Redis'
