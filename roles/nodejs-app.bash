8e240126e30caf423e12cbb350aa9e4d4ac83af2
Nam Nguyen <namnguye@adobe.com>
2014-04-21 22:15:34 -0700

    add nogjs-app role

---
+++ roles/nodejs-app.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function main()
+{
+    local appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    "${appPath}/essential.bash" || exit 1
+
+    "${appPath}/../cookbooks/node-js/recipes/install.bash" || exit 1
+    "${appPath}/../cookbooks/nginx/recipes/install.bash" || exit 1
+    "${appPath}/../cookbooks/redis/recipes/install.bash" || exit 1
+    "${appPath}/../cookbooks/mongodb/recipes/install.bash" || exit 1
+}
+
+main
