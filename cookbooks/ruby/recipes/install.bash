ee425f3ae58c31c0ab3d4e1a38e89023c3e92f55
Nam Nguyen <namnguye@adobe.com>
2014-05-01 15:53:46 -0700

    add Ruby cookbook

---
+++ cookbooks/ruby/recipes/install.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function install()
+{
+    # Clean Up
+
+    rm -rf "${installFolder}"
+    mkdir -p "${installFolder}"
+
+    # Install
+
+    local currentPath="$(pwd)"
+    local tempFolder="$(getTemporaryFolder)"
+
+    unzipRemoteFile "${downloadURL}" "${tempFolder}"
+    cd "${tempFolder}"
+    "${tempFolder}/configure" --prefix="${installFolder}"
+    make
+    make install
+    rm -rf "${tempFolder}"
+    cd "${currentPath}"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
+
+    header 'INSTALLING RUBY'
+
+    install
+}
+
+main "${@}"
