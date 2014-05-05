3d933abc0f360852f54ff1bb5c6c584b60a6efd3
Nam Nguyen <namnguye@adobe.com>
2014-05-05 00:22:19 -0700

    add go cookbook

---
+++ cookbooks/go/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+downloadURL='https://go.googlecode.com/files/go1.2.1.darwin-amd64-osx10.8.tar.gz'
+
+installFolder='/Volumes/Data/Applications/Go'
---
+++ cookbooks/go/recipes/install.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function install()
+{
+    # Clean Up
+
+    rm -rf "${installFolder}"
+    mkdir -p "${installFolder}"
+
+    # Install
+
+    unzipRemoteFile "${downloadURL}" "${installFolder}"
+}
+
+function main()
+{
+    appPath="$(cd "$(dirname "${0}")" && pwd)"
+
+    source "${appPath}/../../../lib/util.bash" || exit 1
+    source "${appPath}/../attributes/default.bash" || exit 1
+
+    header 'INSTALLING GO'
+
+    install
+}
+
+main "${@}"
