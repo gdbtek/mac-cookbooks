40b35e7ce0994d528855d3308e3066d602c84eaa
Nam Nguyen <namnguye@adobe.com>
2014-05-27 21:19:48 -0700

    update nginx version

--- cookbooks/nginx/attributes/default.bash
+++ cookbooks/nginx/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nginxDownloadURL='http://nginx.org/download/nginx-1.7.0.tar.gz'
+nginxDownloadURL='http://nginx.org/download/nginx-1.7.1.tar.gz'
 pcreDownloadURL='ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.35.zip'
 
 installFolder='/Volumes/Data/Applications/Nginx'
