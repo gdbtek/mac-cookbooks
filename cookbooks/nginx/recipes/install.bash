c3655ad07d20a2c4972b1af362bd7a62cb0a0d7d
Nam Nguyen <namnguye@adobe.com>
2014-09-05 16:31:28 -0700

    use BASH_SOURCE

--- cookbooks/nginx/recipes/install.bash
+++ cookbooks/nginx/recipes/install.bash
@@ -38,7 +38,7 @@ function install()
 
 function main()
 {
-    appPath="$(cd "$(dirname "${0}")" && pwd)"
+    appPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     source "${appPath}/../../../lib/util.bash" || exit 1
     source "${appPath}/../attributes/default.bash" || exit 1
