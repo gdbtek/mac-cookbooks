f4938fbca116978fc27fd9c0b960f68ad6f11a97
gdbtek <gdbtek@yahoo.com>
2016-10-22 00:22:38 -0700

    add roles

---
+++ cookbooks/shell-check/recipes/install.bash
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+function install()
+{
+    su -l "${SUDO_USER}" -c "${BREW_INSTALL_FOLDER}/bin/brew install shellcheck"
+    ln -f -s "${BREW_INSTALL_FOLDER}/bin/shellcheck" '/usr/local/bin/shellcheck'
+    displayVersion "$(shellcheck -V)"
+}
+
+function main()
+{
+    local -r installFolder="${1}"
+
+    local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appFolderPath}/../../../libraries/util.bash"
+    source "${appFolderPath}/../../brew/attributes/default.bash"
+
+    checkRequireMacSystem
+    checkRequireRootUser
+
+    header 'INSTALLING SHELL-CHECK'
+
+    # Install
+
+    install
+}
+
+main "${@}"
