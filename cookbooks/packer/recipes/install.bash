4f49091963074df4bfd3c35d3550e991735b4564
gdbtek <gdbtek@yahoo.com>
2016-10-22 22:19:55 -0700

    add packer cookbooks

---
+++ cookbooks/packer/recipes/install.bash
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+function install()
+{
+    # Clean Up
+
+    initializeFolder "${PACKER_INSTALL_FOLDER}"
+    mkdir -p "${PACKER_INSTALL_FOLDER}/bin"
+
+    # Install
+
+    unzipRemoteFile "${PACKER_DOWNLOAD_URL}" "${PACKER_INSTALL_FOLDER}/bin"
+    chown -R "${SUDO_USER}:$(getUserGroupName "${SUDO_USER}")" "${PACKER_INSTALL_FOLDER}"
+    ln -f -s "${PACKER_INSTALL_FOLDER}/bin/packer" '/usr/local/bin/packer'
+
+    # Display Version
+
+    displayVersion "$("${PACKER_INSTALL_FOLDER}/bin/packer" version)"
+}
+
+function main()
+{
+    local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${appFolderPath}/../../../libraries/util.bash"
+    source "${appFolderPath}/../attributes/default.bash"
+
+    checkRequireMacSystem
+    checkRequireRootUser
+
+    header 'INSTALLING PACKER'
+
+    install
+    installCleanUp
+}
+
+main "${@}"
--- roles/macbook-pro.bash
+++ roles/macbook-pro.bash
@@ -5,6 +5,7 @@ function main()
     local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     "${appFolderPath}/../cookbooks/brew/recipes/install.bash"
+    "${appFolderPath}/../cookbooks/packer/recipes/install.bash"
     "${appFolderPath}/../cookbooks/shell-check/recipes/install.bash"
 }
 
