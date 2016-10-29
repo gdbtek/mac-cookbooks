c35b43874501b9f7569120348181f942a2364ce8
gdbtek <gdbtek@yahoo.com>
2016-10-29 01:28:13 -0700

    add aws-cli cookbook

--- cookbooks/aws-cli/recipes/install.bash
+++ cookbooks/aws-cli/recipes/install.bash
@@ -1,14 +1,7 @@
 #!/bin/bash -e
 
-function installDependencies()
-{
-    installPackages 'python'
-}
-
 function install()
 {
-    umask '0022'
-
     # Clean Up
 
     initializeFolder "${AWS_CLI_INSTALL_FOLDER}"
@@ -19,20 +12,13 @@ function install()
 
     unzipRemoteFile "${AWS_CLI_DOWNLOAD_URL}" "${tempFolder}"
     python "${tempFolder}/awscli-bundle/install" -b '/usr/local/bin/aws' -i "${AWS_CLI_INSTALL_FOLDER}"
+    chown -R "${SUDO_USER}:$(getUserGroupName "${SUDO_USER}")" "${AWS_CLI_INSTALL_FOLDER}"
     chmod 755 "${AWS_CLI_INSTALL_FOLDER}/bin/aws"
     rm -f -r "${tempFolder}"
-
-    # Config Profile
-
-    local -r profileConfigData=('__INSTALL_FOLDER__' "${AWS_CLI_INSTALL_FOLDER}")
-
-    createFileFromTemplate "${APP_FOLDER_PATH}/../templates/aws-cli.sh.profile" '/etc/profile.d/aws-cli.sh' "${profileConfigData[@]}"
 
     # Display Version
 
     displayVersion "$("${AWS_CLI_INSTALL_FOLDER}/bin/aws" --version 2>&1)"
-
-    umask '0077'
 }
 
 function main()
@@ -42,12 +28,11 @@ function main()
     source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
     source "${APP_FOLDER_PATH}/../attributes/default.bash"
 
-    checkRequireLinuxSystem
+    checkRequireMacSystem
     checkRequireRootUser
 
     header 'INSTALLING AWS-CLI'
 
-    installDependencies
     install
     installCleanUp
 }
