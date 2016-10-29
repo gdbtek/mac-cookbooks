d22a8e01d4a9eebb0450dcbff3bab7e3f3719328
gdbtek <gdbtek@yahoo.com>
2016-10-29 01:02:54 -0700

    add aws-cli cookbook

---
+++ cookbooks/aws-cli/attributes/default.bash
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+export AWS_CLI_DOWNLOAD_URL='https://s3.amazonaws.com/aws-cli/awscli-bundle.zip'
+
+export AWS_CLI_INSTALL_FOLDER='/Volumes/Data/Applications/aws/aws-cli'
---
+++ cookbooks/aws-cli/recipes/install.bash
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    installPackages 'python'
+}
+
+function install()
+{
+    umask '0022'
+
+    # Clean Up
+
+    initializeFolder "${AWS_CLI_INSTALL_FOLDER}"
+
+    # Install
+
+    local -r tempFolder="$(getTemporaryFolder)"
+
+    unzipRemoteFile "${AWS_CLI_DOWNLOAD_URL}" "${tempFolder}"
+    python "${tempFolder}/awscli-bundle/install" -b '/usr/local/bin/aws' -i "${AWS_CLI_INSTALL_FOLDER}"
+    chmod 755 "${AWS_CLI_INSTALL_FOLDER}/bin/aws"
+    rm -f -r "${tempFolder}"
+
+    # Config Profile
+
+    local -r profileConfigData=('__INSTALL_FOLDER__' "${AWS_CLI_INSTALL_FOLDER}")
+
+    createFileFromTemplate "${APP_FOLDER_PATH}/../templates/aws-cli.sh.profile" '/etc/profile.d/aws-cli.sh' "${profileConfigData[@]}"
+
+    # Display Version
+
+    displayVersion "$("${AWS_CLI_INSTALL_FOLDER}/bin/aws" --version 2>&1)"
+
+    umask '0077'
+}
+
+function main()
+{
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+    source "${APP_FOLDER_PATH}/../attributes/default.bash"
+
+    checkRequireLinuxSystem
+    checkRequireRootUser
+
+    header 'INSTALLING AWS-CLI'
+
+    installDependencies
+    install
+    installCleanUp
+}
+
+main "${@}"
