bd9cf90d97b12e14a7004f45dc01dbfd452ddfe0
gdbtek <gdbtek@yahoo.com>
2016-12-06 09:07:00 -0800

    add node-js cookbook

---
+++ cookbooks/node-js/attributes/default.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+export NODE_JS_INSTALL_FOLDER='/Volumes/Data/Tools/node-js'
+
+# export NODE_JS_VERSION='v0.10.48'
+export NODE_JS_VERSION='latest'
+
+export NODE_JS_INSTALL_NPM_PACKAGES=(
+    'npm'
+    'bower'
+    'grunt-cli'
+)
