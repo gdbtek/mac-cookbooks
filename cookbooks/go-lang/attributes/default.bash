78d2af349f4b2896986d440a4443e7016d99cd13
gdbtek <gdbtek@yahoo.com>
2016-12-06 09:06:52 -0800

    update version

--- cookbooks/go-lang/attributes/default.bash
+++ cookbooks/go-lang/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.7.3.darwin-amd64.tar.gz'
+export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.7.4.darwin-amd64.tar.gz'
 
 export GO_LANG_INSTALL_FOLDER='/Volumes/Data/Tools/go-lang'
--- cookbooks/mongodb/attributes/default.bash
+++ cookbooks/mongodb/attributes/default.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export MONGODB_DOWNLOAD_URL='https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-3.2.10.tgz'
+export MONGODB_DOWNLOAD_URL='https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-3.4.0.tgz'
 
 export MONGODB_INSTALL_FOLDER='/Volumes/Data/Tools/mongodb'
 export MONGODB_INSTALL_DATA_FOLDER="${MONGODB_INSTALL_FOLDER}/data"
