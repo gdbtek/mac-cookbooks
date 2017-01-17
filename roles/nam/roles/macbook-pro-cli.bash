fe236b637a620c0c1157adb19c557c3e45af7810
gdbtek <gdbtek@yahoo.com>
2017-01-16 21:27:40 -0800

    update version

--- roles/nam/roles/macbook-pro-cli.bash
+++ roles/nam/roles/macbook-pro-cli.bash
@@ -14,10 +14,11 @@ function main()
     "${appFolderPath}/../../../cookbooks/nomad/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/packer/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/parallel/recipes/install.bash"
-    "${appFolderPath}/../../../cookbooks/shell-check/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/siege/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/terraform/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/vault/recipes/install.bash"
+
+    "${appFolderPath}/../../../cookbooks/shell-check/recipes/install.bash"
 }
 
 main "${@}"
