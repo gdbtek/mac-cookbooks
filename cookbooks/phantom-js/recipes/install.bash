05757a12d65c95b8ea006cedf5012eb5669d2118
gdbtek <gdbtek@yahoo.com>
2017-01-18 10:21:50 -0800

    bug fixes

--- cookbooks/phantom-js/recipes/install.bash
+++ cookbooks/phantom-js/recipes/install.bash
@@ -9,6 +9,24 @@ function install()
     # Install
 
     unzipRemoteFile "${PHANTOM_JS_DOWNLOAD_URL}" "${PHANTOM_JS_INSTALL_FOLDER}"
+
+    local -r unzipFolder="$(find "${PHANTOM_JS_INSTALL_FOLDER}" -maxdepth 1 -xtype d 2> '/dev/null' | tail -1)"
+
+    if [[ "$(isEmptyString "${unzipFolder}")" = 'true' || "$(wc -l <<< "${unzipFolder}")" != '1' ]]
+    then
+        fatal 'FATAL : multiple unzip folder names found'
+    fi
+
+    if [[ "$(ls -A "${unzipFolder}")" = '' ]]
+    then
+        fatal "FATAL : folder '${unzipFolder}' empty"
+    fi
+
+    moveFolderContent "${unzipFolder}" "${PHANTOM_JS_INSTALL_FOLDER}"
+    rm -f -r "${unzipFolder}"
+
+    # Config
+
     chown -R "${SUDO_USER}:$(getUserGroupName "${SUDO_USER}")" "${PHANTOM_JS_INSTALL_FOLDER}"
     ln -f -s "${PHANTOM_JS_INSTALL_FOLDER}/bin/phantomjs" '/usr/local/bin/phantomjs'
 
