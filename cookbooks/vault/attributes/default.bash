9fee9533a48164297e7d63bad7d3d90f0bd2906a
gdbtek <gdbtek@yahoo.com>
2017-02-08 02:18:38 -0800

    update version

--- cookbooks/vault/attributes/default.bash
+++ cookbooks/vault/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.6.4/vault_0.6.4_darwin_amd64.zip'
+export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.6.5/vault_0.6.5_darwin_amd64.zip'
 
 export VAULT_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/vault'
