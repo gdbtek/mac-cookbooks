2ef1858276b0acf87c8c2586d1edafcb4d3b4d4b
gdbtek <gdbtek@yahoo.com>
2017-02-14 21:13:19 -0800

    update version

--- cookbooks/consul/attributes/default.bash
+++ cookbooks/consul/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/0.7.4/consul_0.7.4_darwin_amd64.zip'
+export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/0.7.5/consul_0.7.5_darwin_amd64.zip'
 
 export CONSUL_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/consul'
