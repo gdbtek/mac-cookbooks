5d1320c4938f079ec476bfb27f9e612091936219
gdbtek <gdbtek@yahoo.com>
2017-02-16 02:36:10 -0800

    bug fixes

--- cookbooks/siege/recipes/install.bash
+++ cookbooks/siege/recipes/install.bash
@@ -2,27 +2,8 @@
 
 function install()
 {
-    # Clean Up
-
-    initializeFolder "${SIEGE_INSTALL_FOLDER_PATH}"
-
-    # Install
-
-    local -r tempFolder="$(getTemporaryFolder)"
-
-    unzipRemoteFile "${SIEGE_DOWNLOAD_URL}" "${tempFolder}"
-    cd "${tempFolder}"
-    "${tempFolder}/configure" --prefix="${SIEGE_INSTALL_FOLDER_PATH}"
-    make
-    make install
-    chown -R "${SUDO_USER}:$(getUserGroupName "${SUDO_USER}")" "${SIEGE_INSTALL_FOLDER_PATH}"
-    cd
-    rm -f -r "${tempFolder}"
-    ln -f -s "${SIEGE_INSTALL_FOLDER_PATH}/bin/siege" '/usr/local/bin/siege'
-
-    # Display Version
-
-    displayVersion "$("${SIEGE_INSTALL_FOLDER_PATH}/bin/siege" --version 2>&1)"
+    compileAndInstallFromSource "${SIEGE_DOWNLOAD_URL}" "${SIEGE_INSTALL_FOLDER_PATH}" "${SIEGE_INSTALL_FOLDER_PATH}/bin/siege" "${SUDO_USER}"
+    displayVersion "$(${SIEGE_INSTALL_FOLDER_PATH}/bin/siege --version 2>&1)"
 }
 
 function main()
