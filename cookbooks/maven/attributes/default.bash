c1b4d50065354952e556ada8956f34ece44286d9
gdbtek <gdbtek@yahoo.com>
2017-04-11 01:36:55 -0700

    update version

--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz'
+export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz'
 
 export MAVEN_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/maven'
