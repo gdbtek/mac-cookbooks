b7bf0ae6033d5425bbbb39feb16c6a1c11723e5d
gdbtek <gdbtek@yahoo.com>
2017-05-25 00:15:27 -0700

    update download url

--- cookbooks/dialog/attributes/default.bash
+++ cookbooks/dialog/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export DIALOG_DOWNLOAD_URL='https://fossies.org/linux/misc/dialog-1.3-20170509.tgz'
+export DIALOG_DOWNLOAD_URL='http://invisible-island.net/datafiles/release/dialog.tar.gz'
 
 export DIALOG_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/dialog'
