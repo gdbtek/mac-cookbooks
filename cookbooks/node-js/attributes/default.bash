f77ad0ce8fbaad9c239935c4ac4d41354bb15297
gdbtek <gdbtek@yahoo.com>
2017-09-04 23:02:21 -0700

    update version

--- cookbooks/node-js/attributes/default.bash
+++ cookbooks/node-js/attributes/default.bash
@@ -2,7 +2,7 @@
 
 export NODE_JS_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/node-js'
 
-# export NODE_JS_VERSION='v0.10.48'
+# export NODE_JS_VERSION='v6.11.2'
 export NODE_JS_VERSION='latest'
 
 export NODE_JS_INSTALL_NPM_PACKAGES=(
