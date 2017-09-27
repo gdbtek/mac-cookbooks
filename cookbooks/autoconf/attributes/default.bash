385c9bb845a7b7fa49e0068057e94c54338b1fcb
gdbtek <gdbtek@yahoo.com>
2017-09-27 00:00:57 -0700

    clean up

--- cookbooks/autoconf/attributes/default.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-export AUTOCONF_DOWNLOAD_URL='http://www.localmsp.org/gnu/autoconf/autoconf-latest.tar.gz'
-
-export AUTOCONF_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/autoconf'
--- cookbooks/automake/attributes/default.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-export AUTOMAKE_DOWNLOAD_URL='http://mirrors.kernel.org/gnu/automake/automake-1.15.1.tar.gz'
-
-export AUTOMAKE_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/automake'
