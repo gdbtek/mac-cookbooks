7944b00952af7096454c2d0af2b8c53cd6fd6227
Nam Nguyen <gdbtek@yahoo.com>
2017-10-15 20:46:31 -0700

    udpate version

--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.1.0/packer_1.1.0_darwin_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.1.1/packer_1.1.1_darwin_amd64.zip'
 
 export PACKER_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/packer'
