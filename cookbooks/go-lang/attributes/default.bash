31f1f591eaf44e80a9f1b3ce5ca806c47639bc44
Nam Nguyen <gdbtek@yahoo.com>
2017-10-27 10:09:18 -0700

    update version

--- cookbooks/go-lang/attributes/default.bash
+++ cookbooks/go-lang/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.9.1.darwin-amd64.tar.gz'
+export GO_LANG_DOWNLOAD_URL='https://storage.googleapis.com/golang/go1.9.2.darwin-amd64.tar.gz'
 
 export GO_LANG_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/go-lang'
