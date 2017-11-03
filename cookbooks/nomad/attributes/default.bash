3edd987283a28bc369e050b35f3bd646dd5cac7e
Nam Nguyen <gdbtek@yahoo.com>
2017-11-03 11:38:59 -0700

    update version

--- cookbooks/nomad/attributes/default.bash
+++ cookbooks/nomad/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.6.3/nomad_0.6.3_darwin_amd64.zip'
+export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.7.0/nomad_0.7.0_darwin_amd64.zip'
 
 export NOMAD_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/nomad'
