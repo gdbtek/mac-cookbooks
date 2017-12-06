b74457b583ab9a5682c63f3c9e9ca6191b1f6bc2
Nam Nguyen <gdbtek@yahoo.com>
2017-12-05 16:54:23 -0800

    update version

--- cookbooks/terraform/attributes/default.bash
+++ cookbooks/terraform/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.10.8/terraform_0.10.8_darwin_amd64.zip'
+export TERRAFORM_DOWNLOAD_URL='https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_darwin_amd64.zip'
 
 export TERRAFORM_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/terraform'
