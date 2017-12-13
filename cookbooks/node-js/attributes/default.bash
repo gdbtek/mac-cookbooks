e7b21e0e4652beaff12f04a9cbe6294a83453e15
Nam Nguyen <gdbtek@yahoo.com>
2017-12-12 23:17:33 -0800

    update node lts version

--- cookbooks/node-js/attributes/default.bash
+++ cookbooks/node-js/attributes/default.bash
@@ -2,7 +2,7 @@
 
 export NODE_JS_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/node-js'
 
-# export NODE_JS_VERSION='v6.11.2'
+# export NODE_JS_VERSION='v8.9.3'
 export NODE_JS_VERSION='latest'
 
 export NODE_JS_INSTALL_NPM_PACKAGES=(
