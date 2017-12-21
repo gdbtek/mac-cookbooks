d2b06acbab0cb1b8baea91a64c4ac5d357abe097
Nam Nguyen <gdbtek@yahoo.com>
2017-12-21 11:50:20 -0800

    update version

--- cookbooks/consul/attributes/default.bash
+++ cookbooks/consul/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/1.0.1/consul_1.0.1_darwin_amd64.zip'
+export CONSUL_DOWNLOAD_URL='https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_darwin_amd64.zip'
 
 export CONSUL_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/consul'
--- cookbooks/nomad/attributes/default.bash
+++ cookbooks/nomad/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.7.0/nomad_0.7.0_darwin_amd64.zip'
+export NOMAD_DOWNLOAD_URL='https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_darwin_amd64.zip'
 
 export NOMAD_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/nomad'
