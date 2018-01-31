d791b07632c812e3e164b52823245553f46a7e6c
Nam Nguyen <gdbtek@yahoo.com>
2018-01-30 22:17:09 -0800

    update version

--- cookbooks/porter/attributes/default.bash
+++ cookbooks/porter/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PORTER_DOWNLOAD_URL='https://github.com/adobe-platform/porter/releases/download/v4.9.0/porter_darwin386'
+export PORTER_DOWNLOAD_URL='https://github.com/adobe-platform/porter/releases/download/v5.0.0/porter_darwin386'
 
 export PORTER_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/porter'
