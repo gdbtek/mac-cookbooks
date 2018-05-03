9f5b6960a6297cbc326373ef1a8e9f0aaa884840
Nam Nguyen <gdbtek@yahoo.com>
2018-05-03 15:40:28 -0700

    update version

--- cookbooks/go-lang/attributes/default.bash
+++ cookbooks/go-lang/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export GO_LANG_DOWNLOAD_URL='https://dl.google.com/go/go1.10.1.darwin-amd64.tar.gz'
+export GO_LANG_DOWNLOAD_URL='https://dl.google.com/go/go1.10.2.darwin-amd64.tar.gz'
 
 export GO_LANG_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/go-lang'
--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.2.2/packer_1.2.2_darwin_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.2.3/packer_1.2.3_darwin_amd64.zip'
 
 export PACKER_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/packer'
