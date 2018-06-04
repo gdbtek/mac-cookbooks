eb2b601d3526719e77db9b5e3fbf7eb57e93d21b
Nam Nguyen <gdbtek@yahoo.com>
2018-06-04 08:36:38 -0700

    update version and remove phantomjs cookbook

--- cookbooks/phantom-js/attributes/default.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-export PHANTOM_JS_DOWNLOAD_URL='https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip'
-
-export PHANTOM_JS_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/phantom-js'
