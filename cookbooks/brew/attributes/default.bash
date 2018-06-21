30b369e1954bb386f9b53cb031c325cf2db42e9d
Nam Nguyen <gdbtek@yahoo.com>
2018-06-20 23:37:24 -0700

    bug fixes

--- cookbooks/brew/attributes/default.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-export BREW_DOWNLOAD_URL='https://github.com/Homebrew/brew/tarball/master'
-
-export BREW_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/brew'
--- cookbooks/brew/recipes/install.bash
+++ cookbooks/brew/recipes/install.bash
@@ -2,45 +2,16 @@
 
 function install()
 {
-    local -r DEFAULT_INSTALL_FOLDER_PATH='/usr/local'
-
-    # Clean Up
-
-    if [[ "${BREW_INSTALL_FOLDER_PATH}" != "${DEFAULT_INSTALL_FOLDER_PATH}" ]]
-    then
-        initializeFolder "${BREW_INSTALL_FOLDER_PATH}"
-    fi
-
-    rm -f -r "$(getCurrentUserHomeFolder)/Library/Caches/Homebrew"
-
-    # Install
-
-    unzipRemoteFile "${BREW_DOWNLOAD_URL}" "${BREW_INSTALL_FOLDER_PATH}" 'tar.gz'
-    chown -R "${SUDO_USER}:$(getUserGroupName "${SUDO_USER}")" "${BREW_INSTALL_FOLDER_PATH}"
-
-    # Update Install Location
-
-    if [[ "${BREW_INSTALL_FOLDER_PATH}" != "${DEFAULT_INSTALL_FOLDER_PATH}" ]]
-    then
-        # Update Global Brew
-
-        createAbsoluteLocalBin 'brew' "${BREW_INSTALL_FOLDER_PATH}/bin/brew"
-
-        # Update Origin Brew
-
-        local -r originConfigData=("${DEFAULT_INSTALL_FOLDER_PATH}" "${BREW_INSTALL_FOLDER_PATH}")
-
-        createFileFromTemplate "${BREW_INSTALL_FOLDER_PATH}/bin/brew" "${BREW_INSTALL_FOLDER_PATH}/bin/brew" "${originConfigData[@]}"
-    fi
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
     # Update
 
-    su -l "${SUDO_USER}" -c "${BREW_INSTALL_FOLDER_PATH}/bin/brew doctor || true"
-    su -l "${SUDO_USER}" -c "${BREW_INSTALL_FOLDER_PATH}/bin/brew update"
+    brew doctor
+    brew update
 
     # Display Version
 
-    displayVersion "$(su -l "${SUDO_USER}" -c "${BREW_INSTALL_FOLDER_PATH}/bin/brew -v")"
+    displayVersion "$(brew -v)"
 }
 
 function main()
@@ -48,10 +19,8 @@ function main()
     local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     source "${appFolderPath}/../../../libraries/util.bash"
-    source "${appFolderPath}/../attributes/default.bash"
 
     checkRequireMacSystem
-    checkRequireRootUser
 
     header 'INSTALLING BREW'
 
