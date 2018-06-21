ff97803f025659d828f789670f750e04cb49d928
Nam Nguyen <gdbtek@yahoo.com>
2018-06-21 00:14:31 -0700

    bug fixes

--- roles/nam/roles/macbook-pro-cli.bash
+++ roles/nam/roles/macbook-pro-cli.bash
@@ -5,12 +5,14 @@ function main()
     local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     "${appFolderPath}/../../../cookbooks/aws-cli/recipes/install.bash"
+    "${appFolderPath}/../../../cookbooks/brew/recipes/uninstall.bash"
     "${appFolderPath}/../../../cookbooks/dialog/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/go-lang/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/jq/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/node-js/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/packer/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/parallel/recipes/install.bash"
+    "${appFolderPath}/../../../cookbooks/shell-check/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/terraform/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/test-ssl/recipes/install.bash"
 }
