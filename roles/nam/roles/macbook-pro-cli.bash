4f02753dd45768bc35b204d5b268c1dca4174907
Nam Nguyen <gdbtek@yahoo.com>
2018-06-21 00:29:38 -0700

    bug fixes

--- roles/nam/roles/macbook-pro-cli.bash
+++ roles/nam/roles/macbook-pro-cli.bash
@@ -4,17 +4,22 @@ function main()
 {
     local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+    # Non-Brew Applications
+
     "${appFolderPath}/../../../cookbooks/aws-cli/recipes/install.bash"
-    "${appFolderPath}/../../../cookbooks/brew/recipes/uninstall.bash"
     "${appFolderPath}/../../../cookbooks/dialog/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/go-lang/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/jq/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/node-js/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/packer/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/parallel/recipes/install.bash"
-    "${appFolderPath}/../../../cookbooks/shell-check/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/terraform/recipes/install.bash"
     "${appFolderPath}/../../../cookbooks/test-ssl/recipes/install.bash"
+
+    # Brew Application
+
+    "${appFolderPath}/../../../cookbooks/brew/recipes/uninstall.bash"
+    sudo -u "${SUDO_USER}" "${appFolderPath}/../../../cookbooks/shell-check/recipes/install.bash"
 }
 
 main "${@}"
