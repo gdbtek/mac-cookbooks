d8d8fed9c16fb2414b8caf07301381d972f9b507
Nam Nguyen <gdbtek@yahoo.com>
2018-06-21 00:19:00 -0700

    update version

--- cookbooks/vault/attributes/default.bash
+++ cookbooks/vault/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.10.2/vault_0.10.2_darwin_amd64.zip'
+export VAULT_DOWNLOAD_URL='https://releases.hashicorp.com/vault/0.10.3/vault_0.10.3_darwin_amd64.zip'
 
 export VAULT_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/vault'
