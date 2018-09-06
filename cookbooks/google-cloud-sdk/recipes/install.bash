b226aed2335c1938994500c2c3ae5b2b12ef6583
Nam Nguyen <gdbtek@yahoo.com>
2018-09-06 11:42:03 -0700

    bug fixes

--- cookbooks/google-cloud-sdk/recipes/install.bash
+++ cookbooks/google-cloud-sdk/recipes/install.bash
@@ -5,14 +5,15 @@ function install()
     # Clean Up
 
     initializeFolder "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}"
-# rm -f '/usr/local/bin/aws'
+    rm -f '/usr/local/bin/gcloud'
 
     # Install
 
     unzipRemoteFile "${GOOGLE_CLOUD_SDK_DOWNLOAD_URL}" "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}"
-    "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}/bin/gcloud" config set disable_usage_reporting true
     "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}/install.sh" --quiet
-    chown -R "${SUDO_USER}:$(getUserGroupName "${SUDO_USER}")" "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}"
+    "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}/bin/gcloud" components update
+    chown -R "${SUDO_USER}:$(getUserGroupName "${SUDO_USER}")" "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}" "$(getUserHomeFolder "${SUDO_USER}")/.config"
+    ln -f -s "${GOOGLE_CLOUD_SDK_INSTALL_FOLDER_PATH}/bin/gcloud" '/usr/local/bin/gcloud'
 
     # Display Version
 
