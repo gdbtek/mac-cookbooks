9913d2d82cd613f69d0a5f3fc1319e5ecc7f4485
Nam Nguyen <gdbtek@yahoo.com>
2018-09-12 10:51:39 -0700

    add chef-client

---
+++ cookbooks/chef-client/recipes/install.bash
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+function installDependencies()
+{
+    if [[ "$(existCommand 'brew')" = 'false' ]]
+    then
+        "${APP_FOLDER_PATH}/../../brew/recipes/install.bash"
+    fi
+}
+
+function install()
+{
+    initializeFolder "$(getCurrentUserHomeFolder)/Library/Caches/Homebrew"
+
+    if [[ "$(existCommand 'java')" = 'true' ]]
+    then
+        brew cask reinstall 'chef/chef/chefdk'
+    else
+        brew cask install 'chef/chef/chefdk'
+    fi
+
+    displayVersion "$(java --version)"
+}
+
+function main()
+{
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+
+    checkRequireMacSystem
+
+    header 'INSTALLING CHEF-CLIENT'
+
+    # Install
+
+    installDependencies
+    install
+}
+
+main "${@}"
---
+++ cookbooks/chef-client/recipes/uninstall.bash
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+function uninstall()
+{
+    if [[ "$(existCommand 'brew')" = 'true' ]]
+    then
+        brew cask uninstall 'chef/chef/chefdk' || true
+    fi
+}
+
+function main()
+{
+    APP_FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    source "${APP_FOLDER_PATH}/../../../libraries/util.bash"
+
+    checkRequireMacSystem
+
+    header 'INSTALLING CHEF-CLIENT'
+
+    # Uninstall
+
+    uninstall
+}
+
+main "${@}"
