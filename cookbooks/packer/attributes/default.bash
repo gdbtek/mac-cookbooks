6c7fd16359583306c5886cb1fa4df12631dda87d
Nam Nguyen <gdbtek@yahoo.com>
2018-10-29 11:18:53 -0700

    update packer version

--- cookbooks/packer/attributes/default.bash
+++ cookbooks/packer/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_darwin_amd64.zip'
+export PACKER_DOWNLOAD_URL='https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_darwin_amd64.zip'
 
 export PACKER_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/packer'
