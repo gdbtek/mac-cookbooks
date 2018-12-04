9e5c7bd7a1ddeeb62de80e464f72c2c2336af756
Nam Nguyen <gdbtek@yahoo.com>
2018-12-04 02:55:46 -0800

    update version

--- cookbooks/maven/attributes/default.bash
+++ cookbooks/maven/attributes/default.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz'
+export MAVEN_DOWNLOAD_URL='http://www.us.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz'
 
 export MAVEN_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/maven'
--- cookbooks/node-js/attributes/default.bash
+++ cookbooks/node-js/attributes/default.bash
@@ -2,7 +2,7 @@
 
 export NODE_JS_INSTALL_FOLDER_PATH='/Volumes/Data/Tools/node-js'
 
-# export NODE_JS_VERSION='v10.13.0'
+# export NODE_JS_VERSION='v10.14.1'
 export NODE_JS_VERSION='latest'
 
 export NODE_JS_INSTALL_NPM_PACKAGES=(
