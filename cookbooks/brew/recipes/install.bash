1b6e7ba1facd7999ad2f6d4e4adcaf2c6a531ae5
Nam Nguyen <gdbtek@yahoo.com>
2019-01-04 11:52:41 -0800

    update lib

--- cookbooks/brew/recipes/install.bash
+++ cookbooks/brew/recipes/install.bash
@@ -2,7 +2,7 @@
 
 function install()
 {
-    yes '' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < '/dev/null'
+    yes '' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
     displayVersion "$(brew -v)"
 }
