4e129270a66f03cfa1f0d43d856026488a1f10d8
Nam Nguyen <gdbtek@yahoo.com>
2019-01-13 19:50:41 -0800

    bug fixes

--- cookbooks/parallel/recipes/install.bash
+++ cookbooks/parallel/recipes/install.bash
@@ -1,26 +1,3 @@
 #!/bin/bash -e
 
-function install()
-{
-    compileAndInstallFromSource "${PARALLEL_DOWNLOAD_URL}" "${PARALLEL_INSTALL_FOLDER_PATH}" "${PARALLEL_INSTALL_FOLDER_PATH}/bin/parallel" "${SUDO_USER}"
-    displayVersion "$(parallel --version)"
-}
-
-function main()
-{
-    local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-    source "${appFolderPath}/../../../libraries/util.bash"
-    source "${appFolderPath}/../attributes/default.bash"
-
-    checkRequireMacSystem
-    checkRequireRootUser
-
-    header 'INSTALLING PARALLEL'
-
-    # Install
-
-    install
-}
-
-main "${@}"
+"$(dirname "${BASH_SOURCE[0]}")/../../../tools/install-brew-application.bash" 'parallel'
--- cookbooks/siege/recipes/install.bash
+++ cookbooks/siege/recipes/install.bash
@@ -1,24 +1,3 @@
 #!/bin/bash -e
 
-function install()
-{
-    compileAndInstallFromSource "${SIEGE_DOWNLOAD_URL}" "${SIEGE_INSTALL_FOLDER_PATH}" "${SIEGE_INSTALL_FOLDER_PATH}/bin/siege" "${SUDO_USER}"
-    displayVersion "$("${SIEGE_INSTALL_FOLDER_PATH}/bin/siege" --version 2>&1)"
-}
-
-function main()
-{
-    local -r appFolderPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-    source "${appFolderPath}/../../../libraries/util.bash"
-    source "${appFolderPath}/../attributes/default.bash"
-
-    checkRequireMacSystem
-    checkRequireRootUser
-
-    header 'INSTALLING SIEGE'
-
-    install
-}
-
-main "${@}"
+"$(dirname "${BASH_SOURCE[0]}")/../../../tools/install-brew-application.bash" 'siege'
