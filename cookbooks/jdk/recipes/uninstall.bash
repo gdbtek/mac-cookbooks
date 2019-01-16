fe2e23cb2e2d84e47fd526a5f64699b1a935f490
Nam Nguyen <gdbtek@yahoo.com>
2019-01-16 12:51:26 -0800

    bug fixes

--- cookbooks/jdk/recipes/uninstall.bash
+++ cookbooks/jdk/recipes/uninstall.bash
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-"$(dirname "${BASH_SOURCE[0]}")/../../../tools/install-brew-application.bash" --application-name 'java' --cask-application 'true'
+"$(dirname "${BASH_SOURCE[0]}")/../../../tools/uninstall-brew-application.bash" --application-name 'java' --cask-application 'true'
 sudo rm -f -r '/Library/Java/JavaVirtualMachines'/openjdk-*.jdk
