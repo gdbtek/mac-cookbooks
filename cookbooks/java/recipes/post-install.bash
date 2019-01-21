a04acb06899e7c9b7ddeae4bb20b39255f33f013
Nam Nguyen <gdbtek@yahoo.com>
2019-01-20 21:41:20 -0800

    bug fixes

--- cookbooks/java/recipes/post-install.bash
+++ cookbooks/java/recipes/post-install.bash
@@ -9,7 +9,7 @@ function main()
 
     checkExistFolder "$(ls -1 -d -t '/Library/Java/JavaVirtualMachines/'openjdk-*.jdk | head -1)/Contents/Home"
 
-    local -r javaHomeConfig='export JAVA_HOME=\"\$(ls -1 -d -t '/Library/Java/JavaVirtualMachines/'openjdk-*.jdk | head -1)/Contents/Home\"'
+    local -r javaHomeConfig="export JAVA_HOME=\"\$(ls -1 -d -t '/Library/Java/JavaVirtualMachines/'openjdk-*.jdk | head -1)/Contents/Home\""
 
     appendToFileIfNotFound '/etc/profile' "${javaHomeConfig}" "${javaHomeConfig}" 'false' 'false' 'true'
     grep -F "${javaHomeConfig}" '/etc/profile'
