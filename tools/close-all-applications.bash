10bfffee98ae6c8d0d04dfd049145b0a8d05cc8f
Nam Nguyen <gdbtek@yahoo.com>
2019-01-27 22:01:00 -0800

    update lib

--- tools/close-all-applications.bash
+++ tools/close-all-applications.bash
@@ -1,20 +1,37 @@
 #!/bin/bash -e
 
+function closeApplicationsOfPath()
+{
+    local -r folderPath="${1}"
+
+    if [[ -d "${folderPath}" ]]
+    then
+        local -r OLD_IFS="${IFS}"
+        IFS=$'\n'
+
+        local -r appNameList=($(
+            find "${folderPath}" \
+                -type d \
+                -name '*.app' \
+                -maxdepth 2 |
+            rev |
+            cut -d '/' -f 1 |
+            rev |
+            sort -f
+        ))
+
+        IFS="${OLD_IFS}"
+
+        closeMacApplications 'CLOSING APPLICATIONS' "${appNameList[@]}"
+    fi
+}
+
 function main()
 {
     source "$(dirname "${BASH_SOURCE[0]}")/../libraries/util.bash"
 
-    local -r OLD_IFS="${IFS}"
-    IFS=$'\n'
-
-    local -r appNameList=($(
-        ls -1 -d /Applications/*.app |
-        cut -d '/' -f 3
-    ))
-
-    IFS="${OLD_IFS}"
-
-    closeMacApplications 'CLOSING APPLICATIONS' "${appNameList[@]}"
+    closeApplicationsOfPath '/Applications'
+    closeApplicationsOfPath "$(getCurrentUserHomeFolder)/Applications"
 }
 
 main "${@}"
