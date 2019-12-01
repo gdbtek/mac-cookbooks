0e6cc8a88af6da410ffff28887b49e8260e1a56b
Nam Nguyen <gdbtek@yahoo.com>
2019-12-01 03:23:39 -0800

    add update software

---
+++ tools/update-software.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+function main()
+{
+    source "$(dirname "${BASH_SOURCE[0]}")/../libraries/util.bash"
+
+    # Validations
+
+    checkRequireMacSystem
+    checkRequireRootUser
+
+    # List and Update
+
+    header 'LISTING AVAILABLE SOFTWARE UPDATES'
+    softwareupdate --list
+
+    header 'UPDATING SOFTWARE'
+    softwareupdate --all --install
+}
+
+main "${@}"
