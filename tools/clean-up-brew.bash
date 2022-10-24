ff57f652d7d87c1b2436e7ab8a18209d2c5b7852
Nam Nguyen <gdbtek@yahoo.com>
2022-10-24 14:39:55 -0700

    update

--- tools/clean-up-brew.bash
+++ tools/clean-up-brew.bash
@@ -13,8 +13,8 @@ function main()
 
     header 'CLEANING UP BREW'
 
-    HOMEBREW_NO_ENV_HINTS=TRUE
-    HOMEBREW_NO_INSTALL_CLEANUP=FALSE
+    export HOMEBREW_NO_ENV_HINTS=TRUE
+    export HOMEBREW_NO_INSTALL_CLEANUP=FALSE
 
     brew cleanup
     initializeFolder "$(brew --cache)"
